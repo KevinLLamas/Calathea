1156164882914eac388a6f0b696661e123d24007
Kevin Llamas <sspannky@hotmail.com>
2022-11-14 20:16:35 -0600

    cambios de diseño

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -1,44 +1,98 @@
 @extends('app')
 @section('content')
-    
-    <!-- End Hero -->
+  <style>.google-map{
+    height:100%;
+    margin-bottom: -8px;
+}
+
+.google-map iframe{
+    width:100%;
+    border:0;
+    filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale"); /* Firefox 10+ */
+    -webkit-filter: grayscale(99%); /* Chrome 19+ & Safari 6+ */
+    -webkit-backface-visibility: hidden;  /* Fix for transition flickering */
+} 
+#map {
+        height: 400px;  /* The height is 400 pixels */
+        width: 100%;  /* The width is the width of the web page */
+       }</style>
     <section id="about" class="about">
-        <img src="{{asset('assets/img/calathea/BANNER.png')}}" class="img-fluid" alt="">
+        <img src="{{asset('assets/img/calathea/banner_principal.png')}}" class="img-fluid" alt="">
+        <div class="container" style="margin-top: -50px;">
+            <div class="card text-left shadow round">
+              <img class="card-img-top" src="holder.js/100px180/" alt="">
+              <div class="card-body row">
+                <div class="form-group col-3" >
+                  <label for="">Fecha</label>
+                  <select class="form-control" name="" id="">
+                    <option>Sab, Mar 2022</option>
+                    <option></option>
+                    <option></option>
+                  </select>
+                </div>
+                <div class="form-group col-3" >
+                  <label for="">Paquete</label>
+                  <select class="form-control" name="" id="">
+                    <option>Paquete 1</option>
+                    <option></option>
+                    <option></option>
+                  </select>
+                </div>
+                <div class="form-group col-2" >
+                  <label for="">Horario</label>
+                  <select class="form-control" name="" id="">
+                    <option>9:00</option>
+                    <option></option>
+                    <option></option>
+                  </select>
+                </div>
+                <div class="form-group col-2 mt-4">
+                  <label for=""></label>
+                  <a name="" id="" class="btn btn-danger mt-1" href="#" role="button">VER PAQUETES</a>
+                </div>
+                <div class="form-group col-2 mt-4">
+                  <label for=""></label>
+                  <a name="" id="" class="btn btn-danger mt-1" href="#" role="button">PRE RESERVAR</a>
+                </div>
+              </div>
+            </div>
+       </div> 
     </section>
-    <section id="about" class="about">
-        
-    </section>
-    <main id="main">
-        <!-- ======= Cliens Section ======= -->
-        <section id="cliens" class="container">
+    <section id="cliens" class="container-fluid">
             <div class="row">
                 <div class="col-md-6">
-                    <img src="{{asset('assets/img/calathea/izq.png')}}" class="img-fluid" alt="">
-                    <img src="{{asset('assets/img/calathea/child.png')}}" class="img-fluid" alt="">
+                    <img src="{{asset('assets/img/calathea/ninio.png')}}" class="img-fluid" alt="">
                     <img src="{{asset('assets/img/calathea/Trazado 27181.png.png')}}" class="img-fluid" alt="">
                 </div>
-                <div class="col-md-6">
-                    NOSOTROS
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ex recusandae. Corrupti delectus nam nihil quo, porro pariatur laboriosam veniam perspiciatis! Ex error quam eos omnis, minus laudantium expedita dignissimos.</p>
-                    <img src="{{asset('assets/img/calathea/derecha.png')}}" class="img-fluid" alt="">
+                <div class="col-md-6 mt-5">
+                    <h1 class="text-primary mx-5">NOSOTROS</h1>
+                    <h2 class="my-5 mx-5 text-secondary">
+                      Calathea Kids es un jardín de eventos infantiles. 
+                      Es el sitio ideal para convertir las fiestas de los niños en experiencias inolvidables de ensueño, donde hacemos también de ti un invitado más en tu propio evento.
+                    </h2>
+                    <br><br><br><br>
+                    <img src="{{asset('assets/img/calathea/derecha.png')}}" class="img-fluid mt-5" alt="">
                 </div>
             </div>
-            
         </section>
-        <section id="cliens" class="container">
+    <main id="main">
+        <!-- ======= Cliens Section ======= -->
+        
+        <section id="cliens" class="container-fluid text-center">
             <div class="row">
                 <div class="col-md-6">
                     <img src="{{asset('assets/img/calathea/galeria.png')}}" class="img-fluid" alt="">
                     <img src="{{asset('assets/img/calathea/izq.png')}}" class="img-fluid" alt="">
+                    <img src="{{asset('assets/img/calathea/lets_party.png')}}" class="img-fluid" alt="">
                 </div>
                 <div class="col-md-6">
                     <img src="{{asset('assets/img/calathea/good_vibes.png')}}" class="img-fluid" alt="">
-                    <img src="{{asset('assets/img/calathea/derecha.png')}}" class="img-fluid" alt="">
+                    <img src="{{asset('assets/img/calathea/ninia_fiesta.png')}}" class="img-fluid" alt="">
                 </div>
             </div>
             
         </section>
-        <section id="cliens" class="container">
+        <!--<section id="cliens" class="container-fluid text-center">
             <div class="row">
                 <div class="col-md-6">
                     <img src="{{asset('assets/img/calathea/lets_party.png')}}" class="img-fluid" alt="">
@@ -48,7 +102,19 @@
                 </div>
             </div>
         </section>
-        <!-- End Contact Section -->
+         End Contact Section -->
+         <div id="map"></div>
     </main>
-    <!-- End #main -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDMOg75K24AhWgbuKKzFQ47q4kuWwufzGI&callback=initMap&signed_in=true" async defer></script>
+    <script>
+    function initMap() {
+      // The location of Uluru
+      var uluru = {lat: 19.512230, lng: -99.268843 };
+      // The map, centered at Uluru 
+      var map = new google.maps.Map(
+          document.getElementById('map'), {zoom: 18, center: uluru});
+      // The marker, positioned at Uluru
+      var marker = new google.maps.Marker({position: uluru, map: map});
+    }
+    </script>
 @endsection
--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,35 +1,31 @@
 
 
 <!-- ======= Footer ======= -->
-<footer class="footer danger-bg mt-5" style="background:#84c5b8" id="">
+<footer class="footer danger-bg" style="background:#93CFC5" id="">
   <div class="footer-top  ">
-      <div class="container ">
-          <div class="row justify-content-around ">
+      <div class="container-fluid">
+          <div class="row justify-content-around ml-3">
               <div class="col-lg-8 mb-3 mb-lg-0 ">
                   <div class="footer-nav-wrap text-white ">
-                      <h2 class="green font font-weight-bold ">CONTÁCTANOS</h2>
+                      <h2 class="green font font-weight-bold mt-5">CONTÁCTANOS</h2>
 
-                      <p class="mb-2 "><img src="img/iconos/footer/Trazado 6932@2x.png " style="width: 2% !important " class="mr-2 ">contacto@carteradeoportunidades.com</p>
-                      <p class="mb-2 "><img src="img/iconos/footer/Grupo 2265@2x.png " style="width: 2% !important " class="mr-2 ">33 31 61 04 74</p>
+                      <p class="mb-2 "><img src="{{asset('assets/img/calathea/correo.png')}}" style="width: 1.3% !important " class="mr-2 ">contacto@calathea.com</p>
+                      <p class="mb-2 "><img src="{{asset('assets/img/calathea/tel.png')}}" style="width: 1.3% !important " class="mr-2 ">33 33 33 33 33</p>
                       <p class="mb-2 ml-4 ">33 38 52 61 32</p>
-                      <p class="mb-2 "><img src="img/iconos/footer/Grupo 2264@2x.png " style="width: 2% !important " class="mr-2 ">Av. Plan de San Luis #1728 Interior 28, Col. Mezquitan Country, Guadalajara, Jalisco,
-
+                      <p class="mb-2 "><img src="{{asset('assets/img/calathea/ubi.png')}}" style="width: 1.3% !important " class="mr-2 ">Av. Plan de San Luis 
                       </p>
-                      <p class="mb-2 ml-4 ">C.P. 44260</p>
+                      <p class="mb-2 ml-4 ">C.P. 4400</p>
                   </div>
               </div>
-              <div class="col-lg-4 ml-auto mb-4 mb-lg-0 ">
-                  <div class="footer-nav-wrap text-white ">
-                      <div class="row ">
-                          <a href="https://www.facebook.com/Cartera-de-Oportunidades-102762029080492 " class="col-4 ">
-                              <img src="img/iconos/footer/Grupo 2262@2x.png " class="w-50 ">
-                          </a>
-                          <a href="https://instagram.com/carteradeoportunidadessapi?igshid=YmMyMTA2M2Y=" class=" col-4 ">
-                              <img src="img/iconos/footer/Grupo 2263@2x.png " class="w-50 ">
-                          </a>
-                          <img src="img/Recurso 2.svg " class="col-4 " style="height: 20%!important; width: 25% !important;flex: 0 0 25%!important; ">
+              <div class="ml-3 mt-5">
+                  <div class="footer-nav-wrap text-white col-12">
+                      <div class="row col-12">
+                          <img src="{{asset('assets/img/calathea/Trazado 31519.png')}}" class=" float-right" >
                       </div>
-
+                      <div class="row col-md-12">
+                          <img src="{{asset('assets/img/calathea/facebook.png')}}" class="w-12 mr-5" >
+                          <img src="{{asset('assets/img/calathea/instagram.png')}}" class="w-12" >
+                      </div>
                   </div>
               </div>
               
